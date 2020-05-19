d7f23c4bdad5daeb92d95c88121ff562a4824aa1
jiek <pinjie.gao@qq.com>
2020-05-19 18:45:02 +0800

    静态代理 与 动态代理的实践 优化注释说明

--- src/com/sample/proxy/Main.java
+++ src/com/sample/proxy/Main.java
@@ -2,8 +2,14 @@ package com.sample.proxy;
 
 /**
  * Created by jiek on 2020/5/19.
- *
+ * <p>
  * 测试动态代理与静态代理模式
+ * <p>
+ * 特点：
+ *  1. 静态代理实现较简单，代理类在编译期生成，效率高。缺点是需要编写大量的代理类。
+ *  2. 动态代理：不要求代理类和委托类实现同一个接口，但是委托类需要实现接口，动态代理类需要实现InvocationHandler接口。
+ *  3. 动态代理：要求代理类实现InvocationHandler接口，通过反射代理方法调用。比较消耗系统性能，但可以减少代理类的数量，使用较灵活。
+ * <p>
  */
 public class Main {
     public static void main(String[] args) {
