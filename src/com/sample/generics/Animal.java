23151d99f8b9c826a4c04295615550188707bc7a
jiek <pinjie.gao@qq.com>
2020-05-26 09:59:27 +0800

    java 1.5引入的泛型 Generics 的理解与验证

---
+++ src/com/sample/generics/Animal.java
@@ -0,0 +1,9 @@
+package com.sample.generics;
+
+/**
+ * Created by jiek on 2020/5/26.
+ *
+ */
+public interface Animal {
+    void eat();
+}
---
+++ src/com/sample/generics/Dog.java
@@ -0,0 +1,11 @@
+package com.sample.generics;
+
+/**
+ * Created by jiek on 2020/5/26.
+ */
+public class Dog implements Animal {
+    @Override
+    public void eat() {
+        System.out.println("Dogs eat bones");
+    }
+}
