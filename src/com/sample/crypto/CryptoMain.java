f21dc711765fb39aad23c01a87221939e5b2eb84
jiek <pinjie.gao@qq.com>
2020-06-09 23:47:43 +0800

    优化 CryptoMain 算法与比较

--- src/com/sample/crypto/CryptoMain.java
+++ src/com/sample/crypto/CryptoMain.java
@@ -1,5 +1,7 @@
 package com.sample.crypto;
 
+import com.sample.util.Util;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -21,39 +23,119 @@ public class CryptoMain {
     private static void testAESEncrypt(String originalString) {
         try {
             //当使用256字节密钥进行加解密可能会有问题，参见：https://blog.csdn.net/dling8/article/details/84061948
-            byte[] key = generateDesKey(128);
+            byte[] key =
+                    hexToByte("867ca046cf63a99632aa5e7a8572a3c5");
+//                    generateDesKey(128);
+            p("DesKey: ", key);
 
-            byte[] bytes1 = "abcdefghijklmnop".getBytes();//new byte[16];
-            IvParameterSpec algorithmParameterSpec = new IvParameterSpec(bytes1);
+            String sks = "AES/CBC/PKCS5Padding";
+            String cryptoMode = "AES";
+            {//方式一
+                byte[] bytes1 = "abcdefghijklmnop".getBytes();//new byte[16];
+//                p(bytes1);
+                IvParameterSpec algorithmParameterSpec = new IvParameterSpec(bytes1);
 
-            SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
+                SecretKeySpec skeySpec = new SecretKeySpec(key, cryptoMode);
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, algorithmParameterSpec);
+                Cipher cipher = Cipher.getInstance(sks);
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec, algorithmParameterSpec);
 
-            byte[] bytes = cipher.doFinal(originalString.getBytes());
-            String ens = new String(bytes);
+                byte[] bytes = cipher.doFinal(originalString.getBytes());
+                p(bytes);
 
-            System.out.println(originalString + " > encrypt : "+ens);
+                System.out.println(originalString + " > encrypt : " + new String(bytes));
 
-            Cipher cipher1 = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher1.init(Cipher.DECRYPT_MODE, skeySpec, algorithmParameterSpec);
+                Cipher cipher1 = Cipher.getInstance(sks);
+                cipher1.init(Cipher.DECRYPT_MODE, skeySpec, algorithmParameterSpec);
 
-            System.out.println("decrypt: "+new String(cipher1.doFinal(bytes)));
+                System.out.println("decrypt: " + new String(cipher1.doFinal(bytes)));
+            }
+            {//方式二
+                Util.splitLine();
+                IvParameterSpec algorithmParameterSpec = new IvParameterSpec(new byte[16]);
+
+                SecretKeySpec skeySpec = new SecretKeySpec(key, cryptoMode);
+
+                Cipher cipher = Cipher.getInstance(sks);
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec, algorithmParameterSpec);
+
+                byte[] bytes = cipher.doFinal(originalString.getBytes());
+                p(bytes);
+
+                System.out.println(originalString + " > encrypt : " + new String(bytes));
+
+                Cipher cipher1 = Cipher.getInstance(sks);
+                cipher1.init(Cipher.DECRYPT_MODE, skeySpec, algorithmParameterSpec);
+
+                System.out.println("decrypt: " + new String(cipher1.doFinal(bytes)));
+            }
+            {//方式三
+                Util.splitLine();
+                SecretKeySpec skeySpec = new SecretKeySpec(key, cryptoMode);
+
+                Cipher cipher = Cipher.getInstance("AES");
+                cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+
+                byte[] bytes = cipher.doFinal(originalString.getBytes());
+                p(bytes);
+
+                System.out.println(originalString + " > encrypt : " + new String(bytes));
+
+                Cipher cipher1 = Cipher.getInstance("AES");
+                cipher1.init(Cipher.DECRYPT_MODE, skeySpec);
+
+                System.out.println("decrypt: " + new String(cipher1.doFinal(bytes)));
+            }
+//            方式一与方式二的差别在IvParameterSpec的参数值不同
+//            方式二与方式三结果相同
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static byte[] generateDesKey(int length) throws Exception {
-        //实例化
-        KeyGenerator kgen = null;
-        kgen = KeyGenerator.getInstance("AES");
-        //设置密钥长度
+    private static byte[] generateDesKey(int length) throws Exception {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
         kgen.init(length);
-        //生成密钥
         SecretKey skey = kgen.generateKey();
         //返回密钥的二进制编码
         return skey.getEncoded();
+    }
+
+    private static void p(String msg, byte[] b) {
+        if (b != null) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < b.length; i++) {
+                sb.append(Integer.toHexString(b[i] & 0xff));
+            }
+            System.out.println(msg+" : "+sb);
+        }
+    }
+    private static void p(byte[] b) {
+        if (b != null) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < b.length; i++) {
+                sb.append(" "+Integer.toHexString(b[i] & 0xff));
+            }
+            System.out.println(sb);
+        }
+    }
+
+    /**
+     * hex转byte数组
+     *
+     * @param hex
+     * @return
+     */
+    public static byte[] hexToByte(String hex) {
+        int m = 0, n = 0;
+        int byteLen = hex.length() / 2;
+        byte[] ret = new byte[byteLen];
+        for (int i = 0; i < byteLen; i++) {
+            m = i * 2 + 1;
+            n = m + 1;
+            int intVal = Integer.decode("0x" + hex.substring(i * 2, m) + hex.substring(m, n));
+            ret[i] = Byte.valueOf((byte) intVal);
+        }
+        return ret;
     }
 }
